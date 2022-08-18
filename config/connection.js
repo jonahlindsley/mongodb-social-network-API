ee11567804e3afc2f072f8f48cc8a10277374238
jonahlindsley <107372935+jonahlindsley@users.noreply.github.com>
2022-08-18 00:15:46 -0500

    Corrected bug

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const is_production = process.env.NODE_ENV;
 
 // Wrap Mongoose around local connection to MongoDB
-const url = is_production ? 'mongodb+srv://jonahlindsley:sapp21HIRE@cluster0.g5pi4fj.mongodb.net/?retryWrites=true&w=majority' : 'mongodb://localhost:27017/social-media-api'
+const url = is_production ? 'mongodb+srv://jonahlindsley:@cluster0.g5pi4fj.mongodb.net/?retryWrites=true&w=majority' : 'mongodb://localhost:27017/social-media-api'
 mongoose.connect(url, {
 
   useNewUrlParser: true,
